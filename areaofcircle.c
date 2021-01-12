db2e1b2f38f210d04003cd9650abb10c1882588e
Ilya Matusov <matusovi2036@my.sduhsd.net>
2021-01-12 03:40:56 +0000

    Act 2

---
+++ areaofcircle.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+// for testing only - do not change
+void getTestInput(int argc, char* argv[], float* a, int* b)
+{
+  if (argc == 3) {
+    sscanf(argv[1], "%f", a);
+    sscanf(argv[2], "%d", b);
+  }
+}
+
+
+
+// add your areaOfCircle function here - it must NOT printf, instead it must
+// return the result to be printed in main
+float areaOfCircle(float radi)
+
+float result = acos(-1) * radi * radi;
+return result;
+
+
+int main(int argc, char* argv[]) 
+{
+  // the two variables which control the number of times areaOfCircle is called
+  // in this case 5.2, 6.2, 7.2
+  float start = 5.2;
+  int reps = 3;
+
+  // for testing only - do not change
+  getTestInput(argc, argv, &start, &reps);
+
+  printf("calculating area of circle starting at %f, and ending at %f\n", start, start+reps-1);
+  
+  // add your code below to call areaOfCircle function with values between
+  for (int i = 0; i < reps; i++) 
+  {
+  	aa = areaOfCircle(start)
+        start = start + 1
+	printf("Area: %d\n", aa)
+  }
+  // start and end  
+}
